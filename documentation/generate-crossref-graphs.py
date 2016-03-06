df3e0150a5e7a693cbe065ed7a66b915f5454cbc
Maïeul Rouquette <maieul@maieul.net>
2016-03-06 12:01:36 +0100

    avoir un fichier .dot par type d'entrée

---
+++ documentation/generate-crossref-graphs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This file generate the inheritance graphs (.dot and .pdf file) from the .bib file examples.
+# To do it, its create temporary .tex file, calling a specific .bib file (as https://github.com/plk/biber/issues/110 was rejected).
+# It is licensed on GPL 3 licenses.
+# https://www.gnu.org/licenses/gpl-3.0.fr.html
+# Copyright : Maïeul Rouquette 2016-…
+
+import os
+
+
+#List all the files on the current directory
+directory_files = os.listdir(".")
+
+#Loop on them, and for the .bib file, generate the .tex, .dot and .pdf file
+for file_name in directory_files:
+    basename, ext = os.path.splitext(file_name)
+
+    if ext != '.bib':#only the .bib file
+        continue
+
+    # write the .tex file content
+    tex_file_name = basename + ".tex"
+    tex_file_content = "\documentclass{article}\n\
+    \\usepackage[bibstyle=bookinother]{biblatex}\n\
+    \\bibliography{" + file_name + "}\n\
+    \\begin{document}\n\
+    \\nocite{*}\n\
+    \end{document}"
+    tex_file_file = open(tex_file_name, "w")
+    tex_file_file.write(tex_file_content)
+    tex_file_file.close()
+
+    # generate the .bcf, .dot and .pdf file
+    os.system("pdflatex " + basename)
+    os.system("biber -output-format=dot --dot-include=crossref,field " + basename)
+    os.system("dot -Tpdf " + basename + ".dot " + "-o " + basename + ".pdf")
+
+    # by security, delete the .tex file, to distribute it
+    os.remove(tex_file_name)
